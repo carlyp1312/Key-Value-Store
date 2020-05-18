869ae186ecfe9725379ecf3a73adaa850f5727a7
pranavkn <Pranav@Pranavs-MacBook-Pro-2.local>
2020-05-17 20:49:34 -0700

    added put code for key value operations

--- api.py
+++ api.py
@@ -6,7 +6,8 @@ from requests.exceptions import ConnectionError
 #create flask object and set debugging option to true
 app = Flask(__name__)
 app.config["DEBUG"] = True
-store = {}
+key_value_store = {} # keeps track of all key value pairs
+metadata_store = {} # keeps track of all causal history of this replica
 
 # this file represents just one replica
 # we can get this replica's vector clock position through its socket address position in the view environmental variable
@@ -22,7 +23,6 @@ if(views is not None):
 socket_address = os.getenv('SOCKET_ADDRESS', default=None)
 if(socket_address is not None):
     baseUrl = 'http://' + str(socket_address)
-    isMain = False
 
 vector_clock = {}
 buffer = []
@@ -31,7 +31,7 @@ buffer = []
 
 def initialize():
     for v in views_list:
-        vector_clock[v] = 0
+        vector_clock[v] = 0 # v is the socket address (key), 0 is the corresponding value
 
 def pointwiseMax(vc):
     new_vector_clock = []
@@ -41,145 +41,50 @@ def pointwiseMax(vc):
 
 #assumption: incremented before it was sent REMEMBER TO DO THAT 
 #might need to add another parameter
-def canDeliver(vc):
+def canDeliver(vc, sa):
     #vc must be <= vector_clock in every position besides senders position
+    for i in range(len(vc)):
+        if():
+            
+            
     #vc at senders position == vector_clock at senders position plus one 
     #else queue 
 
-
-
-
-
-
-
-# def isLessThan(vc):
-#     bools = []
-#     bools2 = []
-
-#     for i in range(len(vector_clock)):
-#         if(vector_clock[i] <= vc[i]):
-#             bools[i] = True
-#         else:
-#             bools[i] = False
-    
-#     for i in range(len(vector_clock)):
-#         if(vc[i] <= vector_clock[i]):
-#             bools2[i] = True
-#         else:
-#             bools2[i] = False
-
-#     if False in bools & False in bools2:
-#         islessthan = "independent"
-
-#     allTrue = True
-#     for b in bools2:
-#         if b == False:
-#             allTrue = False 
-#             break
-    
-#     if allTrue == True:
-#         islessthan = "causally (before)dependent"
-        
-
-# can bools have all trues? that means the receiving
-# vector clock, vector_clock, happens before the passed
-# in vector clock, vc. is this possible?
-# it makes sense bools2 can have all trues, since the 
-# passed-in vector clock, vc, happens before the receiving
-# vector clock, vector_clock, which seems consistent with
-# a diagram of a process sending a request and its vector clock,
-# vc, to another process with its own vector clock, vector_clock.
-# i.e. a diagram like this seems consistent with vc happening
-# before vector_clock, not vector_clock happening before vc.
-
-# A [2,2,4]
-# B [1,2,3]
-# A < B [F, T, F]
-# B < A [T, T, T] 
-
+# This endpoint below will specify which replica getting the curl request, 
+# when the @app.route functionality gets the socket address of the replica
+# from the curl request endpoint.
 @app.route('/key-value-store/<key>', methods=['PUT', 'GET', 'DELETE'])
 def main(key):
-    if isMain == False:
-        #PUT request
-        saddress = # replica-socket-address extraction from curl command
-        vc = # causal metadata extraction from curl command
-        # for v in views_list:
-        #     if (saddress == v):
-        #         canDeliver(vc, saddress)
+    #PUT request
+    metadata = request.json.get('causal-metadata') # causal metadata extraction from curl command
+    if metadata == '':
+        initialize() # set all vector clock positions to zero
         if request.method == 'PUT':
-            try:
-                r = requests.put(baseUrl + '/key-value-store/' + str(key), json={'value': request.json.get('value')})
-                return r.content, r.status_code 
-            except ConnectionError:
-                data = '{"error":"Main instance is down","message":"Error in PUT"}'
-                status_code = 503
-                return data, status_code
-        
-        #GET request        
-        if request.method == 'GET':
-            try:
-                r = requests.get(baseUrl + '/key-value-store/' + str(key))
-                return r.content, r.status_code
-            except ConnectionError:
-                data = '{"error":"Main instance is down","message":"Error in GET"}'
-                status_code = 503
-                return data, status_code
-
-        #DELETE request
-        if request.method == 'DELETE':
-            #requests.delete(envvar)     
-            try:
-                r = requests.delete(baseUrl + '/key-value-store/' + str(key))
-                return r.content, r.status_code
-            except ConnectionError:
-                data = '{"error":"Main instance is down","message":"Error in DELETE"}'
-                status_code = 503
-                return data, status_code
+            # first generates causal metadata
+            for i in vector_clock:
+                if i == socket_address:
+                    vector_clock[i] += 1
+            key_value_store[key] = request.json.get('value') #updates local key value store
+            metadata_store.append(vector_clock) #updates local causal metadata store
+            data = '{"message":"Added successfully", "causal-metadata": ' + vector_clock + '}'
+            status_code = 201
+            # tores , broadcoast to obroadcastcas
+            for i in vector_clock:
+                if i != socket_address: # broadcast to every replica other than current replica
+                    r = requests.put(baseUrl + '/key-value-store/' + str(key), json={'value': request.json.get('value')})
+            return data, status_code
+        else:
+            if request.method == 'GET':
+                data = {"error":"Non-existent key","message":"Error in GET"}
+                status_code = 404
+            if request.method == 'DELETE':
+                data = {"error":"Non-existent key","message":"Error in DELETE"}
+                status_code = 404
     else:
-        if request.method == 'PUT':
-            value = request.json.get('value')
-            if value != None:
-                if key in store:
-                    #update 
-                    store[key] = value
-                    data = '{"message":"Updated successfully","replaced":true}'
-                    status_code = 200
-                else:
-                    if len(key) > 50:
-                        #key too long
-                        data ='{"error":"Key is too long","message":"Error in PUT"}'
-                        status_code = 400
-                    else:
-                        #add 
-                        store[key] = value  
-                        data = '{"message":"Added successfully","replaced":false}'
-                        status_code = 201
-            else: 
-                #value missing
-                data = '{"error":"Value is missing","message":"Error in PUT"}'
-                status_code = 400
-        
-        #GET request        
-        if request.method == 'GET':
-            if key in store:
-                data ='{"doesExist":true,"message": "Retrieved successfully","value": "' + str(store[key]) + '"}'
-                status_code = 200
-            else:
-                data = '{"doesExist":false,"error":"Key does not exist","message":"Error in GET"}'
-                status_code = 404
-
-        #DELETE request
-        if request.method == 'DELETE':
-            if key in store:
-                del store[key]
-                data = '{"doesExist":true,"message":"Deleted successfully"}'
-                status_code = 200
-            else:
-                data = '{"doesExist":false,"error":"Key does not exist","message":"Error in DELETE"}'
-                status_code = 404
-
-        return data, status_code
-
+        # for i in vector_clock:
+        #     if i == socket_address:
+        #         vector_clock[i] += 1
+        canDeliver(metadata, socket_address)
 
 @app.route('/key-value-store-view', methods=['PUT', 'GET', 'DELETE'])
 def viewmain():
