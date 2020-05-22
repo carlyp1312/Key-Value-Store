cdbd08836d4a4d6d7d3b8cc7b0862f46cff8cc31
Carl Yahata-Pettersson <cyahatap@Carls-MacBook-Pro.local>
2020-05-22 03:56:34 -0700

    passes all test cases, added logic for causal delivery, need more test cases

--- api.py
+++ api.py
@@ -6,8 +6,8 @@ from requests.exceptions import ConnectionError
 app = Flask(__name__) # create flask object
 app.config["DEBUG"] = True # set debugging option to true
 key_value_store = {} # keeps track of all key value pairs
-metadata_store = [] # keeps track of causal history
-disconnected_replicas = []
+metadata_store = {} # keeps track of causal history
+queue = [] # keeps track of requests that can not currently be delivered
 
 views = os.getenv('VIEW', default=None) #forwarding address = ip address of the main instance
 views_list = []
@@ -26,53 +26,92 @@ vector_clock = {}
 for v in views_list: # initialize vector_clock only when this replica is first created, not when it is reconnected
     vector_clock[str(v)] = 0 # v is the socket address (key), 0 is the corresponding value
 
-@app.route('/store', methods=['GET'])
+@app.route('/store', methods=['GET']) # get key_value_store
 def get_store():
     data = {"store": key_value_store}
     return data
 
-@app.route('/broadcast/<key>', methods=['PUT'])
+@app.route('/store1', methods=['GET']) # get metadata_store
+def get_store1():
+    data = {"store": metadata_store}
+    return data
+
+def canDeliver1(sender_vc): # when client sends a request to this replica
+    if sender_vc == '':
+        return True
+    else:
+        for position in vector_clock:
+            if (sender_vc[position] > vector_clock[position]):
+                return False
+        return True
+
+def canDeliver2(sender_sa, sender_vc): # when replica sends a request to another replica
+    for position in vector_clock:
+        if position == sender_sa:
+            if sender_vc[position] != vector_clock[position] + 1:
+                return False
+        else:
+            if sender_vc[position] > vector_clock[position]:
+                return False
+    return True
+
+@app.route('/broadcast/<key>', methods=['PUT']) # write broadcast (replica to replica)
 def broadcast(key):
     metadata = request.json.get('causal-metadata')
-    #IMPLEMENT CAUSAL CONSISTENCY HERE
     if request.method == 'PUT': # regardless of the metadata being empty or not, increment the vector clock by one in the sender's position
-        vector_clock[request.json.get('sender')] += 1 # increment local vector clock by one in the sender's position
-        key_value_store[key] = request.json.get('value') # updates local key value store
-        metadata_store.append(vector_clock) # updates local causal metadata store
+        deliverable = canDeliver2(request.json.get('sender'), request.json.get('vector-clock'))
+        if deliverable == True:
+            vector_clock[request.json.get('sender')] += 1 # increment local vector clock by one in the sender's position
+            key_value_store[key] = request.json.get('value') # updates local key value store
+            metadata_store[str(len(metadata_store))] = vector_clock.copy()
+        else:
+            queue.append(request) # add this request to queue if it is not deliverable
 
-@app.route('/key-value-store/<key>', methods=['PUT', 'GET', 'DELETE'])
+# When a replica is reconnected, what happens to its vector clock? Current implementation is that it is updated as if it was sending and receiving messages
+# Queue behavior?
+@app.route('/key-value-store/<key>', methods=['PUT', 'GET', 'DELETE']) # main endpoint (client to replica)
 def store_main(key):
-    #IMPLEMENT CAUSAL CONSISTENCY HERE
     for v in views_list:
         if v != socket_address:
             try: # when a replica reconnects to the network, update its store by getting one from a running replica
                 requests.put('http://' + str(v) + '/key-value-store-view', timeout=1, json={'socket-address': socket_address})
                 r = requests.get('http://' + str(v) + '/store')
-                key_value_store.update(r.json()['store']) # update local key value store
+                key_value_store.update(r.json()['store']) # update local key value store upon reconnection
+                m = requests.get('http://' + str(v) + '/store1')
+                metadata_store.update(m.json()['store']) # update local metadata store upon reconnection
+                vector_clock.update(metadata_store[str(len(metadata_store) - 1)]) # make sure the vector clock is the most recent one
             except: # replica is down, no need to do anything here
                 continue
-    if request.method == 'PUT':
+
+    if request.method == 'PUT': 
         metadata = request.json.get('causal-metadata') # causal metadata extraction from curl command
-        vector_clock[socket_address] += 1 # increment local vector clock by one in its own position
-        if key in key_value_store:
-            status_code = 200 # update existing key's value
+        deliverable = canDeliver1(metadata) # if false, add to buffer. if not, proceed with request
+        if deliverable == True:
+            if request in queue: # remove this request from queue if it exists
+                queue.remove(request)
+            vector_clock[socket_address] += 1 # increment local vector clock by one in its own position
+            if key in key_value_store:
+                status_code = 200 # update existing key's value
+            else:
+                status_code = 201 # add new key value pair
+            key_value_store[key] = request.json.get('value') # updates local key value store
+            data = {"message": "Added successfully", "causal-metadata": vector_clock}
+            metadata_store[str(len(metadata_store))] = vector_clock.copy()
+            for replica in vector_clock.keys():
+               if replica != socket_address:
+                   try: # broadcast request to every other running replica
+                      requests.put('http://' + replica + '/broadcast/' + key, timeout=1, json={'sender': socket_address, 'value': request.json.get('value'), 'vector-clock': vector_clock})
+                   except ConnectionError: # replica is either disconnected or killed
+                        for v in views_list:
+                            if replica != v:
+                                try: # regardless if the replica is disconnected or killed, remove it from every running replica's view list
+                                    requests.delete('http://' + v + '/key-value-store-view', timeout=1, json={'socket-address': replica})
+                                except:
+                                    continue
+            return data, status_code
         else:
-            status_code = 201 # add new key value pair
-        key_value_store[key] = request.json.get('value') # updates local key value store
-        metadata_store.append(vector_clock) # updates local causal metadata store
-        data = {"message": "Added successfully", "causal-metadata": vector_clock}
-        for replica in vector_clock.keys():
-           if replica != socket_address:
-               try: # broadcast request to every other running replica
-                  requests.put('http://' + replica + '/broadcast/' + key, timeout=1, json={'sender': socket_address, 'value': request.json.get('value'), 'causal-metadata': metadata})
-               except ConnectionError: # replica is either disconnected or killed
-                    for v in views_list:
-                        if replica != v:
-                            try: # regardless if the replica is disconnected or killed, remove it from every running replica's view list
-                                requests.delete('http://' + v + '/key-value-store-view', timeout=1, json={'socket-address': replica})
-                            except:
-                                continue
-        return data, status_code
+            queue.append(request) # add this request to queue if it is not deliverable
+
     if request.method == 'GET':
         if key in key_value_store.keys():
             data = {"message": "Value retrieved successfully", "value": key_value_store[key], "causal-metadata": vector_clock}
@@ -98,7 +137,7 @@ def view_main():
             vi += str(v)
             if idx != len(views_list) - 1:
                 vi += ',' # test script wants a specific format
-        data = {"message": "View retrieved successfully", "view": vi, "store": key_value_store, "causal-metadata": metadata_store}  # last two aren't necessary, but helpful info
+        data = {"message": "View retrieved successfully", "view": vi, "store": key_value_store, "causal-metadata": metadata_store, "vector-clock": vector_clock}  # last two aren't necessary, but helpful info
         status_code = 200
         return data, status_code
 
@@ -113,4 +152,4 @@ def view_main():
             status_code = 404
         return data, status_code
 
-app.run(host="0.0.0.0", port=8085) 
+app.run(host="0.0.0.0", port=8085)
